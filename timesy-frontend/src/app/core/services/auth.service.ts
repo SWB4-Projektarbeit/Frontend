112580d2a9f61f0658614b868638f8745dab7668
Zeanon <thezeanon@gmail.com>
2026-06-24 12:08:43 +0200

    fix logout

--- timesy-frontend/src/app/core/services/auth.service.ts
+++ timesy-frontend/src/app/core/services/auth.service.ts
@@ -1,15 +1,19 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
 
 @Injectable({ providedIn: 'root' })
 export class AuthService {
   private readonly backendUrl = 'http://localhost:8215';
 
+  constructor(private http: HttpClient) {}
+
   login() {
-    const redirectUri = encodeURIComponent(window.location.href);
-    window.location.href = `${this.backendUrl}/api-timesy/login?redirect_uri=${redirectUri}`;
+    globalThis.location.href = `${this.backendUrl}/api-timesy/login?redirect_uri=${globalThis.location.href}`;
   }
 
   logout() {
-    window.location.href = `${this.backendUrl}/api-timesy/logout`;
+    this.http.get(`${this.backendUrl}/api-timesy/logout`).subscribe(() => {
+        globalThis.location.reload();
+    });
   }
 }
